144c9b0d4a994d6bafa6aa11e1d89d468637dd59
aidenkim05 <aidenkim05@gmail.com>
2025-09-03 09:51:15 +0900

    Add files via upload

---
+++ 0901_김병준1.py
@@ -0,0 +1,44 @@
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+print()
+
+print('Hello World!')
+print('Hello', 'World')
+print('Hello'+'World')  #문자열 연결연산자 "+"
+print(100)
+print(2+4)
+'''
+print("반짝 " *10)
+
+print(100)
+print(10 + 20)
+print(10 - 20)
+print(10 * 20)
+print(10/20)
+print(123*8765)
+print(10 + 20 * 30)
+
+print(20/10)    #실수
+print(20//10)   #정수
+print(25%10)    #나머지
+
+x = 100     #좌 = 우
+y = 200     #변수 = 변수, 수식, 값
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x ** 2)
+x = 5
+print(x ** 2)
+'''
+
+
+
+
+
+
+
+
+
